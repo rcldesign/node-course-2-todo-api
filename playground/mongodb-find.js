24a93ea67986ff9da3face815a72ba92e57a2e00
Topher <topher@topherdub.com>
2018-06-13 12:41:17 -0700

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos').find().count().then((docs) => {
+    //     console.log(`Todos: ${docs}`);
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos: ', err);
+    // });
+
+    db.collection('Users').find({name: 'Topher'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos: ', err);
+    });
+
+    // client.close();
+});
