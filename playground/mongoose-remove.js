71c9c6a01d383b2a2c3c22509890d21666a0e741
Topher <topher@topherdub.com>
2018-06-15 20:00:24 -0700

    Added PATCH tests

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove()
+// Todo.findByIdAndRemove()
+
+Todo.findByIdAndRemove('5b244296ba20f08a6e9cb241').then((todo) => {
+    console.log(todo);
+});
